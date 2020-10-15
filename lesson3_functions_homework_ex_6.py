f2edf77b1ab5c26f39284306cf5cea149497122f
Nikita <lukonenko.nikitka@mail.ru>
2020-10-15 16:16:21 +0300

    Доделал 6 задание для третьего урока, просто я неправильно закоммитил д.з.

--- lesson3_functions_homework_ex_6.py
+++ lesson3_functions_homework_ex_6.py
@@ -1,16 +1,20 @@
-def right_words():
-    words = 'I gulay s собакой'
+def right_words(words):
     words = words.split()
-    good_letter = []
     good_word = []
     def right_letter(words):
         for word in words:
+            good_letter = []
             for letter in word:
                 if ord(letter) in range(96, 123):
-                    good_word.append(word)
-        return good_word
+                    good_letter.append(letter)
+                    if len(good_letter) == len(word):
+                        good_word.append(word)
+                        good_letter = []
+        a = good_word
+        a = str(a)
+        a = print(a.title())
+        return a
     return right_letter(words)
-a = right_words()
-a = str(a)
-a = a.title()
-print(a)
+
+
+right_words(input('Введите слова на латинском и других языках ЧЕРЕЗ ПРОБЕЛ для сортировки : '))
