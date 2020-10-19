40a666186a482ffd0d3e88e05b8c70716f76be29
Nikita <lukonenko.nikitka@mail.ru>
2020-10-19 19:32:25 +0300

    ДЗ к 4 уроку переделал 2 и 7 задания .

--- Lesson_4_helpfull_instruments_homework_2.py
+++ Lesson_4_helpfull_instruments_homework_2.py
@@ -1,5 +1,6 @@
 import random
 first_list = [random.randrange(1, 101) for i in range(10)]
 print(first_list)
-second_list = [first_list[n] for n in range(len(first_list)) if first_list[n] > first_list[n - 1]]
+n = 1
+second_list = [first_list[n] for n in range(n, len(first_list)) if first_list[n] > first_list[n - 1]]
 print(second_list)
--- Lesson_4_helpfull_instruments_homework_7.py
+++ Lesson_4_helpfull_instruments_homework_7.py
@@ -1,11 +1,14 @@
 def generator():
-    list_generator = [num for num in range(1, 10)]
+    list_generator = input('Введите числа через пробел').split()
     for el in list_generator:
+        el = float(el)
         fact_list = []
         fact = 1
         while el > 1:
             fact = fact * el
             el = el - 1
+            if el == 0:
+                fact = 1
         fact_list.append(fact)
         yield fact_list
 
