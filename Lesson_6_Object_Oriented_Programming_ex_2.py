af304f912764db9dd45952cd96011b5c54bba19f
Nikita <lukonenko.nikitka@mail.ru>
2020-10-25 21:40:49 +0300

    Переделал 2 задание для 6 урока.

--- Lesson_6_Object_Oriented_Programming_ex_2.py
+++ Lesson_6_Object_Oriented_Programming_ex_2.py
@@ -1,8 +1,15 @@
 class Road:
-    def __init__(self, _length, _width,  massa_per_meter, thickness):
+    def __init__(self, _length, _width, thickness):
         self._length = _length
         self._width = _width
-        print(f'{int((self._length * self._width * massa_per_meter * thickness) / 1000)} т.')
+        self.thickness = thickness
 
 
-asphalt_area = Road(20, 5000, 25, 5)
+    def massa_per_meter(self):
+        mass_per_meter = 0.01 * self.thickness
+        massa = self._length * self._width * mass_per_meter * self.thickness
+        return massa
+
+
+asphalt_mass = Road(20, 5000, 5)
+print(f'{int(asphalt_mass.massa_per_meter())} т.')
